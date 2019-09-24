4a8ab8a47767f47ab4294c2039fba3fefb8bea96
Aengus McMillin <aengusmcmillin@gmail.com>
2019-09-23 19:21:10 -0700

    update home page contents

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,9 +11,13 @@ const IndexPage = () => {
             </h2>
             <p>
                 Cheddar Games is a one-man shop building the <Link to="/gouda">Gouda</Link> engine and games with that engine.
+            </p>
+            <p>
                 Follow development on the <Link to="/blog">devblog</Link>
             </p>
-            <br/>
+            <p>
+                Checkout future blog/engine/game plans on <a href="https://trello.com/cheddargames">Trello</a>
+            </p>
             <h2>
                 Contact
             </h2>
